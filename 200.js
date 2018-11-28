ac12b825eebbdb56e2084c8ea2ed2023c3e969eb
Daniel Cousens <dcousens@users.noreply.github.com>
2018-11-28 11:01:27 +1100

    uses statuses dependency instead

--- 200.js
+++ 200.js
@@ -1,12 +1,12 @@
 var dhttp = require('./')
-var httpStatus = require('http-status-codes')
+var httpStatus = require('statuses/codes')
 
 module.exports = function only200 (options, callback) {
   dhttp(options, function (err, result) {
     if (err) return callback(err)
     if (result.statusCode < 200 || result.statusCode >= 300) {
       var message = result.body
-      if (!message) message = httpStatus.getStatusText(result.statusCode)
+      if (!message) message = httpStatus[result.statusCode]
 
       return callback(new Error(message))
     }
