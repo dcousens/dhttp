7d3aec716f55e4e0bba65c3333635475b3cd2ae7
Daniel Cousens <dcousens@users.noreply.github.com>
2018-11-28 11:23:08 +1100

    use httpbin for testing

--- test/index.js
+++ test/index.js
@@ -1,103 +1,70 @@
 const dhttp = require('../')
-const express = require('express')
 const tape = require('tape')
+const tf = require('typeforce')
 
-const app = express()
-const http = require('http')
+const EMPTY_TYPE = tf.value(null)
+const JSON_TYPE = tf.compile({
+  slideshow: {
+    slides: tf.arrayOf({ title: tf.String }, { length: 2 })
+  }
+})
+const ECHO_TYPE = tf.compile({
+  args: tf.Object,
+  data: tf.String,
+  files: tf.Object,
+  form: tf.Object,
+  headers: tf.Object,
+  origin: tf.String,
+  url: tf.String
+})
 
-const TENS = Buffer.alloc(99, 10) // 99x 10's
 const vectors = [
-  { path: '/text', value: 'foobar' },
-  { path: '/json', value: { 'foo': 'bar' } },
-  { path: '/buffer', value: TENS },
-  { path: '/echo', method: 'POST', body: { foo: 1 }, value: { foo: 1 } },
-  { path: '/echo', method: 'POST', body: [ 1 ], value: [ 1 ] },
-  { path: '/echo/raw', method: 'POST', body: TENS, value: TENS },
-  { path: '/echo', method: 'POST', body: '{"foo":1}', headers: { 'content-type': 'application/json' }, value: { foo: 1 } },
-  { path: '/echo', method: 'POST', body: { foo: 1 }, headers: { 'content-type': 'application/json' }, value: { foo: 1 } }
+  { path: '/json', type: JSON_TYPE },
+  { path: '/bytes/4', type: tf.BufferN(4) },
+  { path: '/bytes/30', type: tf.BufferN(30) },
+  { path: '/stream-bytes/40', type: tf.BufferN(40) },
+  { path: '/delay/6', statusCode: 200, type: ECHO_TYPE },
+  { path: '/status/200', statusCode: 200, type: EMPTY_TYPE },
+  { path: '/status/300', statusCode: 300, type: EMPTY_TYPE },
+  { path: '/status/404', statusCode: 404, type: EMPTY_TYPE },
+  { path: '/stream/3', errorRegex: /Unexpected token {/ }
 ]
 
-app.get('/text', function (req, res) {
-  res.status(200).send('foobar')
-})
+vectors.forEach((v) => {
+  let { method, errorRegex, statusCode, path, headers, body, type } = v
+  method = method || 'GET'
+  statusCode = statusCode || 200
 
-app.get('/json', function (req, res) {
-  res.status(200).send({ 'foo': 'bar' })
-})
-
-app.get('/bad/json', function (req, res) {
-  res.status(200)
-  res.setHeader('Content-Type', 'application/json')
-  res.write('{ foo: bad }')
-  res.end()
-})
-
-app.get('/bad/content', function (req, res) {
-  res.status(200)
-  res.setHeader('Content-Type', 'ohno')
-  res.write('foobar')
-  res.end()
-})
-
-app.get('/buffer', function (req, res) {
-  res.status(200).send(TENS)
-})
-
-app.post('/echo/raw', require('body-parser').raw(), function (req, res) {
-  res.status(200).send(req.body)
-})
-
-app.post('/echo', require('body-parser').json(), function (req, res) {
-  res.status(200).send(req.body)
-})
-
-const server = http.createServer(app)
-server.listen(8080)
-
-tape('dhttp', function (t) {
-  t.plan(3 * vectors.length + 2 + 3 + 3 + 1)
-
-  vectors.forEach((v) => {
+  tape(`${path} OK`, function (t) {
     dhttp({
-      method: v.method || 'GET',
-      url: 'http://localhost:8080' + v.path,
-      headers: v.headers,
-      body: v.body
+      method,
+      url: `http://httpbin.org/${path}`,
+      headers,
+      body
     }, function (err, res) {
-      t.error(err)
-      t.equal(res.statusCode, 200)
-      t.deepEqual(res.body, v.value)
+      if (err) {
+        if (errorRegex) {
+          t.throws(function () {
+            throw err
+          }, errorRegex)
+        } else {
+          t.error(err)
+        }
+
+        return t.end()
+      }
+
+      t.equal(res.statusCode, statusCode, `status ${statusCode}`)
+      if (type) {
+        t.ok(tf(v.type, res.body), 'matches expected type')
+      }
+
+      t.end()
     })
   })
+})
 
-  dhttp({
-    method: 'GET',
-    url: 'http://localhost:8080/missing'
-  }, function (err, res) {
-    t.error(err)
-    t.equal(res.statusCode, 404)
-  })
-
-  dhttp({
-    method: 'GET',
-    url: 'http://localhost:8080/bad/json'
-  }, function (err, res) {
-    t.ok(err)
-    t.throws(() => {
-      throw err
-    }, /Unexpected token f/)
-    t.equal(res, undefined)
-  })
-
-  dhttp({
-    method: 'GET',
-    url: 'http://localhost:8080/bad/content'
-  }, function (err, res) {
-    t.error(err)
-    t.equal(res.statusCode, 200)
-    t.equal(res.body, null)
-  })
-
+tape('timeout', (t) => {
   dhttp({
     method: 'GET',
     url: 'http://255.255.255.0/',
@@ -106,9 +73,6 @@ tape('dhttp', function (t) {
     t.throws(() => {
       throw err
     }, /ETIMEDOUT/)
+    t.end()
   })
-})
-
-tape.onFinish(function () {
-  server.close()
 })
