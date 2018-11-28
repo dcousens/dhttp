e6731ad08148e605adfc44f63015880f29263c65
Daniel Cousens <dcousens@users.noreply.github.com>
2018-11-28 13:17:15 +1100

    restore testing coverage to >90%

--- test/index.js
+++ test/index.js
@@ -7,26 +7,34 @@ const JSON_TYPE = tf.compile({
     slides: tf.arrayOf({ title: tf.String }, { length: 2 })
   }
 })
-const ECHO_TYPE = tf.compile({
-  args: tf.Object,
-  data: tf.String,
-  files: tf.Object,
-  form: tf.Object,
-  headers: tf.Object,
-  origin: tf.String,
-  url: tf.String
-})
 
+function echo (back) {
+  return tf.compile(Object.assign({
+    args: tf.Object,
+    data: tf.String,
+    files: tf.Object,
+    form: tf.Object,
+    headers: tf.Object,
+    origin: tf.String,
+    url: tf.String
+  }, back))
+}
+
+const v = tf.value
 const vectors = [
-  { path: '/status/200', statusCode: 200, type: tf.value('') },
-  { path: '/status/300', statusCode: 300, type: tf.value('') },
-  { path: '/status/404', statusCode: 404, type: tf.value('') },
+  { path: '/status/200', statusCode: 200, type: v('') },
+  { path: '/status/300', statusCode: 300, type: v('') },
+  { path: '/status/404', statusCode: 404, type: v('') },
+  { path: '/anything', method: 'POST', type: echo({ data: v('text') }), body: 'text' },
+  { path: '/anything', method: 'POST', type: echo({ json: { foo: v(1) } }), body: { foo: 1 } },
+  { path: '/anything', method: 'POST', type: echo({ json: { foo: v(1) } }), body: { foo: 1 }, headers: { 'content-type': 'application/json' } },
+  { path: '/anything', method: 'POST', type: echo({ json: { foo: v(1) } }), body: '{"foo":1}', headers: { 'content-type': 'application/json' } },
   { path: '/json', type: JSON_TYPE },
   { path: '/bytes/4', type: tf.BufferN(4) },
   { path: '/bytes/30', type: tf.BufferN(30) },
   { path: '/stream-bytes/40', type: tf.BufferN(40) },
   { path: '/stream/3', errorRegex: /Unexpected token {/ },
-  { path: '/delay/9', statusCode: 200, type: ECHO_TYPE }
+  { path: '/delay/9', statusCode: 200, type: echo() }
 ]
 
 vectors.forEach((v) => {
