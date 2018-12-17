995bd1e64c0146db9dc296b39bc1723fafbaad59
Daniel Cousens <dcousens@users.noreply.github.com>
2018-12-18 00:09:28 +1100

    use const in 200 variant

--- 200.js
+++ 200.js
@@ -1,13 +1,11 @@
-var dhttp = require('./')
-var httpStatus = require('statuses/codes.json')
+const dhttp = require('./')
+const httpStatus = require('statuses/codes.json')
 
 module.exports = function only200 (options, callback) {
   dhttp(options, function (err, result) {
     if (err) return callback(err)
     if (result.statusCode < 200 || result.statusCode >= 300) {
-      var message = result.body
-      if (!message) message = httpStatus[result.statusCode]
-
+      const message = result.body || httpStatus[result.statusCode]
       return callback(new Error(message))
     }
 
